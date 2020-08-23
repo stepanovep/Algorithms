16864b1c9768b8d5776cb14c7f2eb5dd59ea161f
stepanovep <stepanovep@gmail.com>
2020-08-24 01:24:32 +0900

    data structure: Trie implementation changed - using hashMap instead of arrayList

--- src/datastructures/Trie.java
+++ src/datastructures/Trie.java
@@ -1,64 +1,39 @@
 package datastructures;
 
-class Trie {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trie {
 
     static class TrieNode {
-        // R links to node children
-        private final TrieNode[] links;
-
-        private static final int R = 26;
-        private boolean isEnd;
-
-        public TrieNode() {
-            links = new TrieNode[R];
-        }
-
-        public boolean containsKey(char ch) {
-            return links[ch - 'a'] != null;
-        }
-
-        public TrieNode get(char ch) {
-            return links[ch - 'a'];
-        }
-
-        public void put(char ch, TrieNode node) {
-            links[ch - 'a'] = node;
-        }
-
-        public void setEnd() {
-            isEnd = true;
-        }
-
-        public boolean isEnd() {
-            return isEnd;
-        }
+        private final Map<Character, TrieNode> links = new HashMap<>();
+        private boolean isWord = false;
     }
 
     private final TrieNode root;
 
-    Trie(TrieNode root) {
+    public Trie(TrieNode root) {
         this.root = root;
     }
 
-    public void insert(String word) {
+    private void insert(String word) {
         TrieNode node = root;
-        for (int i = 0; i < word.length(); i++) {
-            char currentChar = word.charAt(i);
-            if (!node.containsKey(currentChar)) {
-                node.put(currentChar, new TrieNode());
-            }
 
-            node = node.get(currentChar);
+        for (char ch: word.toCharArray()) {
+            if (!node.links.containsKey(ch)) {
+                node.links.put(ch, new TrieNode());
+            }
+            node = node.links.get(ch);
         }
-        node.setEnd();
+        node.isWord = true;
     }
 
     private TrieNode searchPrefix(String prefix) {
         TrieNode node = root;
         for (int i = 0; i < prefix.length(); i++) {
             char cur = prefix.charAt(i);
-            if (node.containsKey(cur)) {
-                node = node.get(cur);
+            if (node.links.containsKey(cur)) {
+                node = node.links.get(cur);
             } else {
                 return null;
             }
@@ -67,12 +42,12 @@ class Trie {
         return node;
     }
 
-    public boolean search(String word) {
+    private boolean search(String word) {
         TrieNode node = searchPrefix(word);
-        return node != null && node.isEnd();
+        return node != null && node.isWord;
     }
 
-    public boolean startsWith(String prefix) {
+    private boolean startsWith(String prefix) {
         TrieNode node = searchPrefix(prefix);
         return node != null;
     }
@@ -80,12 +55,14 @@ class Trie {
     public static void main(String[] args) {
         Trie trie = new Trie(new TrieNode());
 
-        trie.insert("le");
-        trie.insert("leet");
-        trie.insert("code");
+        trie.insert("asdf");
+        trie.insert("fff");
+        trie.insert("ffsdf");
 
-        System.out.println(trie.search("leet"));
-        System.out.println(trie.startsWith("lee"));
+        System.out.println(trie.startsWith("ff"));
+
+        System.out.println(trie.search("asd"));
+        System.out.println(trie.search("asdf"));
     }
 }
 
