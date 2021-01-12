fcb243b7dc68715aa6ab26b2bf31b479b1f86a33
Egor Stepanov <stepanovep@gmail.com>
2021-01-12 21:08:07 +0300

    data structure: Segment tree

--- src/datastructures/SegmentTree.java
+++ src/datastructures/SegmentTree.java
@@ -1,8 +1,5 @@
 package datastructures;
 
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-
 /**
  * Classic segment tree implementation
  *
@@ -11,19 +8,17 @@ import static java.lang.Math.min;
 public class SegmentTree {
 
     private static final int MAX_VAL = 100_001;
-
-    private final int[] t;
     private final int n;
+
+    private final int[] t = new int[4*MAX_VAL];
 
     public SegmentTree(int[] a) {
         this.n = a.length;
-        this.t = new int[4*n];
-        build(a, 1, 0, n-1);
+        build(a, 0, 0, n-1);
     }
 
     public SegmentTree(int n) {
         this.n = n;
-        this.t = new int[4*n];
     }
 
     public void build(int[] a, int v, int tl, int tr) {
@@ -31,56 +26,58 @@ public class SegmentTree {
             t[v] = a[tl];
         } else {
             int mid = (tl + tr) / 2;
-            build(a, v*2, tl, mid);
-            build(a, v*2+1, mid+1, tr);
-            t[v] = t[v*2] + t[v*2+1];
+            build(a, 2*v+1, tl, mid);
+            build(a, 2*v+2, mid+1, tr);
+            t[v] = t[2*v+1] + t[2*v+2];
         }
     }
 
     public int query(int left, int right) {
-        return query(1, 0, n-1, left, right);
+        return query(0, 0, n-1, left, right);
     }
 
     private int query(int v, int tl, int tr, int l, int r) {
-        if (l > r) {
+        if (tr < l || tl > r) {
             return 0;
         }
-        if (l == tl && r == tr) {
+
+        if (tl >= l && tr <= r) {
             return t[v];
         }
 
         int tm = (tl + tr) / 2;
-        int leftQuery = query(v*2, tl, tm, l, min(r, tm));
-        int rightQuery = query(v*2+1, tm+1, tr, max(l, tm+1), r);
-        return leftQuery + rightQuery;
+        int leftResult = query(2*v+1, tl, tm, l, r);
+        int rightResult = query(2*v+2, tm+1, tr, l, r);
+
+        return leftResult + rightResult;
     }
 
     public void update(int pos, int newVal) {
-        update(1, 0, n-1, pos, newVal);
+        update(0, 0, n-1, pos, newVal);
     }
 
     private void update(int v, int tl, int tr, int pos, int newVal) {
         if (tl == tr) {
             t[v] = newVal;
-        } else {
-            int tm = (tl + tr) / 2;
-            if (pos <= tm) {
-                update(v*2, tl, tm, pos, newVal);
-            } else {
-                update(v*2+1, tm+1, tr, pos, newVal);
-            }
-            t[v] = t[v*2] + t[v*2+1];
+            return;
         }
+
+        int tm = (tl + tr) / 2;
+        if (pos <= tm) {
+            update(2*v+1, tl, tm, pos, newVal);
+        } else {
+            update(2*v+2, tm+1, tr, pos, newVal);
+        }
+        t[v] = t[2*v+1] + t[2*v+2];
     }
 
     public static void main(String[] args) {
-        int[] a = new int[] {1, 4, -3};
-        int n = a.length;
+        int[] a = new int[] {1, 4, -3, 5};
         SegmentTree st = new SegmentTree(a);
 
         System.out.println(st.query(1, 2));
-        st.update(0, 10);
-        System.out.println(st.query(0, 2));
+        st.update(2, 10);
+        System.out.println(st.query(1, 2));
     }
 
 }
